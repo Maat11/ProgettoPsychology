52f09fd2cfb4f41bc1d5800bf5e97af39b0c5b56
Maat11 <enzocapuo@gmail.com>
2026-05-23 23:07:22 +0200

    add comment

--- src/PazienteDAO.java
+++ src/PazienteDAO.java
@@ -6,6 +6,9 @@ public interface PazienteDAO {
 	
 	void popolaTabella(DefaultTableModel model, String cognome) throws PersonalException;
 	
+//	private void popolaTabellaFirstMode(DefaultTableModel model) throws PersonalException;
+//	private void popolaTabellaSecondMode(DefaultTableModel model, String cognome) throws PersonalException;
+	
 	int prendiIdPaziente(String codiceFiscale) throws PersonalException;
 	
 }
